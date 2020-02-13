2daea2539f9474b05e61ca422e0e376cadbbd0b9
刘鑫恺 <31811138+cleariove@users.noreply.github.com>
2020-02-13 12:13:49 +0800

    后台完成分页，分类转发功能

--- src/tmall/servlet/BaseBackServlet.java
+++ src/tmall/servlet/BaseBackServlet.java
@@ -1,8 +1,51 @@
 package tmall.servlet;
 
+import tmall.dao.CategoryDAO;
+import tmall.util.Page;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.Method;
 
 public class BaseBackServlet extends HttpServlet
 {
+    protected CategoryDAO categoryDAO = new CategoryDAO();
 
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        //分页默认从0开始，每页显示5项
+        int start = 0;
+        int count = 5;
+        if(req.getParameter("page.start") != null)
+            start = Integer.parseInt(req.getParameter("page.start"));
+        if(req.getParameter("page.count") != null)
+            count = Integer.parseInt(req.getParameter("page.count"));
+        Page page = new Page(start,count);
+
+        //通过反射调用了CategoryServlet中对应的方法
+        String method = (String) req.getAttribute("method");
+        String redirect = "";
+        try
+        {
+            Method m = this.getClass().getDeclaredMethod(method,HttpServletRequest.class,
+                    HttpServletResponse.class,Page.class);
+            redirect = (String) m.invoke(this,req,resp,page);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        //根据方法返回的字符串进行跳转
+        if(redirect.startsWith("@"))
+            resp.sendRedirect(redirect.substring(1));
+        else if(redirect.startsWith("%"))
+            resp.getWriter().print(redirect.substring(1));
+        else
+            req.getRequestDispatcher(redirect).forward(req,resp);
+
+    }
 }
--- src/tmall/servlet/CategoryServlet.java
+++ src/tmall/servlet/CategoryServlet.java
@@ -1,6 +1,51 @@
 package tmall.servlet;
 
+
+import tmall.bean.Category;
+import tmall.util.Page;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CategoryServlet extends BaseBackServlet
 {
+
+    public String list(HttpServletRequest request, HttpServletResponse response, Page page)
+    {
+        List<Category> categories = categoryDAO.list(page.getStart(),page.getCount());
+        int total = categoryDAO.getTotal();
+        page.setTotal(total);
+        request.setAttribute("categories",categories);
+        request.setAttribute("page",page);
+        return "admin/listCategory.jsp";
+    }
+
+    public String add(HttpServletRequest request, HttpServletResponse response, Page page)
+    {
+        return null;
+    }
+
+    public String delete(HttpServletRequest request, HttpServletResponse response, Page page)
+    {
+        int id = Integer.parseInt(request.getParameter("id"));
+        categoryDAO.delete(id);
+        return "@admin/listCategory.jsp";
+    }
+
+    public String edit(HttpServletRequest request, HttpServletResponse response, Page page)
+    {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Category c = categoryDAO.get(id);
+        request.setAttribute("c",c);
+        return "admin/editCategory.jsp";
+    }
+
+    public String update(HttpServletRequest request, HttpServletResponse response, Page page)
+    {
+
+    }
+
 
 }
