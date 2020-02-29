5d08b4497c6a3bcf1c6f721847c8da3233caaa46
cleariove <765357429@qq.com>
2020-02-29 21:12:16 +0800

    完成注册部分

--- src/tmall/servlet/ForeServlet.java
+++ src/tmall/servlet/ForeServlet.java
@@ -1,6 +1,8 @@
 package tmall.servlet;
 
+import org.springframework.web.util.HtmlUtils;
 import tmall.bean.Category;
+import tmall.bean.User;
 import tmall.util.Page;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,5 +17,22 @@ public class ForeServlet extends BaseForeServlet {
         productDAO.fillByRow(categories);
         request.setAttribute("categories",categories);
         return "home.jsp";
+    }
+
+    public String register(HttpServletRequest request, HttpServletResponse response, Page page)
+    {
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+        name = HtmlUtils.htmlEscape(name);
+        if(userDAO.isExist(name))
+        {
+            request.setAttribute("msg","用户名已经存在");
+            return "register.jsp";
+        }
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        userDAO.add(user);
+        return "@registerSuccess.jsp";
     }
 }
