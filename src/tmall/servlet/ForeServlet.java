386c344d0eb4e42860192384510fd90b6cf5b841
cleariove <765357429@qq.com>
2020-03-01 13:51:50 +0800

    完成模态窗口

--- src/tmall/servlet/ForeServlet.java
+++ src/tmall/servlet/ForeServlet.java
@@ -10,23 +10,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 public class ForeServlet extends BaseForeServlet {
-    public String home(HttpServletRequest request, HttpServletResponse response, Page page)
-    {
+    public String home(HttpServletRequest request, HttpServletResponse response, Page page) {
         List<Category> categories = categoryDAO.list();
         productDAO.fill(categories);
         productDAO.fillByRow(categories);
-        request.setAttribute("categories",categories);
+        request.setAttribute("categories", categories);
         return "home.jsp";
     }
 
-    public String register(HttpServletRequest request, HttpServletResponse response, Page page)
-    {
+    public String register(HttpServletRequest request, HttpServletResponse response, Page page) {
         String name = request.getParameter("name");
         String password = request.getParameter("password");
         name = HtmlUtils.htmlEscape(name);
-        if(userDAO.isExist(name))
-        {
-            request.setAttribute("msg","用户名已经存在");
+        if (userDAO.isExist(name)) {
+            request.setAttribute("msg", "用户名已经存在");
             return "register.jsp";
         }
         User user = new User();
@@ -36,13 +33,12 @@ public class ForeServlet extends BaseForeServlet {
         return "@registerSuccess.jsp";
     }
 
-    public String login(HttpServletRequest request, HttpServletResponse response, Page page)
-    {
+    public String login(HttpServletRequest request, HttpServletResponse response, Page page) {
         String name = request.getParameter("name");
         name = HtmlUtils.htmlEscape(name);
         String password = request.getParameter("password");
-        User user = userDAO.get(name,password);
-        if(user==null){
+        User user = userDAO.get(name, password);
+        if (user == null) {
             request.setAttribute("msg", "账号密码错误");
             return "login.jsp";
         }
@@ -50,26 +46,42 @@ public class ForeServlet extends BaseForeServlet {
         return "@forehome";
     }
 
-    public String logout(HttpServletRequest request, HttpServletResponse response, Page page)
-    {
+    public String logout(HttpServletRequest request, HttpServletResponse response, Page page) {
         request.getSession().removeAttribute("user");
         return "@forehome";
     }
 
-    public String product(HttpServletRequest request, HttpServletResponse response, Page page)
-    {
+    public String product(HttpServletRequest request, HttpServletResponse response, Page page) {
         int pid = Integer.parseInt(request.getParameter("pid"));
         Product product = productDAO.get(pid);
         List<ProductImage> productSingleImages = productImageDAO.list(product, ProductImageDAO.type_single);
-        List<ProductImage> productDetailImages = productImageDAO.list(product,ProductImageDAO.type_detail);
+        List<ProductImage> productDetailImages = productImageDAO.list(product, ProductImageDAO.type_detail);
         product.setProductSingleImages(productSingleImages);
         product.setProductDetailImages(productDetailImages);
         List<PropertyValue> propertyValues = propertyValueDAO.list(pid);
         List<Review> reviews = reviewDAO.list(pid);
         productDAO.setSaleAndReviewNumber(product);
-        request.setAttribute("p",product);
-        request.setAttribute("reviews",reviews);
-        request.setAttribute("pvs",propertyValues);
+        request.setAttribute("p", product);
+        request.setAttribute("reviews", reviews);
+        request.setAttribute("pvs", propertyValues);
         return "product.jsp";
+    }
+
+    public String checkLogin(HttpServletRequest request, HttpServletResponse response, Page page) {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null)
+            return "%error";
+        return "%success";
+    }
+
+    public String loginAjax(HttpServletRequest request, HttpServletResponse response, Page page)
+    {
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+        User user = userDAO.get(name,password);
+        if(user == null)
+            return "%error";
+        request.getSession().setAttribute("user",user);
+        return "%success";
     }
 }
